91c680708b2917f5a5227f8de9cbd8e198e842a7
fullstackdeveloper710 <fullstackdeveloper710@gmail.com>
2025-01-09 11:23:16 +0530

    add some changes

--- server.js
+++ server.js
@@ -1,3 +1,70 @@
+// const express = require('express');
+// const http = require('http');
+// const cors = require('cors');
+// const { Server } = require('socket.io');
+// require('dotenv').config();
+
+// const app = express();
+// const server = http.createServer(app);
+
+// // app.use(cors({
+// //   origin: process.env.CLIENT_URL || 'http://localhost:3000',
+// //   methods: ['GET', 'POST'],
+// // }));
+
+// app.use(cors({
+//   origin: [process.env.CLIENT_URL || 'http://localhost:3000'],
+//   methods: ['GET', 'POST'],
+//   credentials: true,
+// }));
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: process.env.CLIENT_URL || 'http://localhost:3000',
+//     methods: ['GET', 'POST'],
+//   },
+// });
+
+// const users = {};
+// const adminId = 'admin';
+
+// io.on('connection', (socket) => {
+//   // console.log(`User connected: ${socket.id}`);
+//   socket.on('register', (userId) => {
+//     users[userId] = socket.id;
+
+//     if (userId !== adminId) {
+//       const connectedUsers = Object.keys(users).filter((id) => id !== adminId);
+//       io.to(users[adminId]).emit('userList', connectedUsers);
+//     }
+
+//     console.log('Registered users:', users);
+//   });
+
+//   socket.on('sendMessage', ({ senderId, receiverId, message }) => {
+//     const receiverSocketId = users[receiverId];
+//     if (receiverSocketId) {
+//       io.to(receiverSocketId).emit('receiveMessage', { senderId, message,receiverId });
+//     }
+//   });
+
+//   socket.on('disconnect', () => {
+//     // console.log(`User disconnected: ${socket.id}`);
+//     const disconnectedUser = Object.keys(users).find(
+//       (userId) => users[userId] === socket.id
+//     );
+//     delete users[disconnectedUser];
+//     const connectedUsers = Object.keys(users).filter((id) => id !== adminId);
+//     io.to(users[adminId]).emit('userList', connectedUsers);
+//   });
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// server.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
@@ -7,29 +74,25 @@ require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
 
-// app.use(cors({
-//   origin: process.env.CLIENT_URL || 'http://localhost:3000',
-//   methods: ['GET', 'POST'],
-// }));
-
 app.use(cors({
-  origin: [process.env.CLIENT_URL || 'http://localhost:3000'],
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
   methods: ['GET', 'POST'],
-  credentials: true,
 }));
 
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL || 'http://localhost:3000',
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST'], 
   },
 });
 
 const users = {};
 const adminId = 'admin';
 
+// WebSocket Events
 io.on('connection', (socket) => {
-  // console.log(`User connected: ${socket.id}`);
+  console.log(`User connected: ${socket.id}`);
+
   socket.on('register', (userId) => {
     users[userId] = socket.id;
 
@@ -44,23 +107,28 @@ io.on('connection', (socket) => {
   socket.on('sendMessage', ({ senderId, receiverId, message }) => {
     const receiverSocketId = users[receiverId];
     if (receiverSocketId) {
-      io.to(receiverSocketId).emit('receiveMessage', { senderId, message,receiverId });
+      io.to(receiverSocketId).emit('receiveMessage', { senderId, message, receiverId });
     }
   });
 
   socket.on('disconnect', () => {
-    // console.log(`User disconnected: ${socket.id}`);
     const disconnectedUser = Object.keys(users).find(
       (userId) => users[userId] === socket.id
     );
     delete users[disconnectedUser];
     const connectedUsers = Object.keys(users).filter((id) => id !== adminId);
     io.to(users[adminId]).emit('userList', connectedUsers);
+    console.log(`User disconnected: ${socket.id}`);
   });
 });
 
-const PORT = process.env.PORT || 5000;
+// Welcome Route
+app.get('/', (req, res) => {
+  res.send('Server is working. Welcome!');
+});
 
+// Start Server
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
